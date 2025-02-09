8a9b416f4a875542b2ab6b91d64c2f920937dfea
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2025-02-10 00:27:56 +0300

    Created a test application with errors

---
+++ index.js
@@ -0,0 +1,7 @@
+const message = "Hello, world!"
+
+function printMessage() {
+  console.log(message)
+}
+
+printMessage()
---
+++ utils.js
@@ -0,0 +1,10 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+console.log(add(5, 10))
+console.log(subtract(15, 5))
